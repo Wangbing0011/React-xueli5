62ae962bad188da7eb5db4430e1dd7bff3308b04
王冰 <45160595+Wangbing0011@users.noreply.github.com>
2020-11-10 19:50:07 +0800

    Add files via upload

---
+++ precache-manifest.0ca4ccd351ac37de0091dfdfa5f0d5a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf37483150a05cc234545acdf2ff142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a0e4a1f107e75074884",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "9cf43c66a7aae2f8f33d",
+    "url": "/static/css/main.cb19ef75.chunk.css"
+  },
+  {
+    "revision": "3a0e4a1f107e75074884",
+    "url": "/static/js/2.ece5433e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.ece5433e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf43c66a7aae2f8f33d",
+    "url": "/static/js/main.5175f840.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
